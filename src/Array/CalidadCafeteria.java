f01006a6b76b7b2381469317f49c7a535d7c4749
Maria Guadalupe Villasana <22670010@iguala.tecnm.mx>
2023-05-28 12:02:17 -0600

    "Se crean arreglos"

--- src/Array/CalidadCafeteria.java
+++ src/Array/CalidadCafeteria.java
@@ -4,28 +4,28 @@ package Array;
 import java.security.SecureRandom;
 
 public class CalidadCafeteria {
-    static int generaRespuesta(int n) {
-        SecureRandom random = new SecureRandom();
-        return 1 + random.nextInt();
-
-    }
-
     public static void main(String[] args) {
-        final int CANT_ALUMNOS = 30;
-        int[] calificaciones = new int[6];
+        SecureRandom random = new SecureRandom();
+        int[] calif = new int[6];
+        final int ALUMNOS = 30;
+        int calidad;
+        calif[1] = 0;
+        calif[2] = 0;
+        calif[3] = 0;
+        calif[4] = 0;
+        calif[5] = 0;
 
-        for (int i = 0; i < CANT_ALUMNOS; i++) {
-            calificaciones[generaRespuesta(5)] += 1;
-
+        for (int i = 0; i < ALUMNOS; i++) {
+            calidad = random.nextInt(5) + 1;
+            calif[calidad] = calif[calidad] + 1;
         }
-
-        System.out.println("Se recibieron " + CANT_ALUMNOS + "respuestas");
-
-
-        for (int i = 0; i < calificaciones.length; i++) {
-            System.out.println();
-        }
+        System.out.println("Solo a "+calif[5]+" alumnos calificaron como (5) EXCELENTE la comida de la cafeteria");
+        System.out.println("Solo a "+calif[4]+" alumnos calificaron como (4) BUENO la comida de la cafeteria");
+        System.out.println("Solo a "+calif[3]+" alumnos calificaron como (3) REGULAR la comida de la cafeteria");
+        System.out.println("Solo a "+calif[2]+" alumnos calificaron como (2) MALO la comida de la cafeteria");
+        System.out.println("Solo a "+calif[1]+" alumnos calificaron como (1) PESIMO la comida de la cafeteria");
     }
-
 }
+
+
 
